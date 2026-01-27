3d726595685dea2e1441e910135c1096839d8f31
Sebastian Dröge <sebastian@centricular.com>
2026-01-27 17:11:36 +0200

    Update various other dependencies

--- examples/subscriber.rs
+++ examples/subscriber.rs
@@ -21,7 +21,7 @@ use gst::prelude::*;
 mod cli;
 
 #[derive(Debug, Display, Error)]
-#[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
+#[display("Received error from {}: {} (debug: {:?})", src, error, debug)]
 struct ErrorMessage {
     src: String,
     error: String,
